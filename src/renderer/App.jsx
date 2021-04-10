bf1edc605fdeb28cc14a169f56f27ae26c7d87e0
the-wulv <michaiac18@sch.gr>
2021-04-10 13:29:21 +0300

    Moved the App component into a separate file

---
+++ src/renderer/App.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import Grid from './components/Grid';
+
+export default class App extends Component {
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return <Grid numberOfPads={100} />
+	}
+}
--- src/renderer/renderer.jsx
+++ src/renderer/renderer.jsx
@@ -2,12 +2,6 @@ import './style.css';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-import Grid from './components/Grid';
-class App extends Component {
-
-	render(){
-		return <Grid numberOfPads={100} />
-	}
-}
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
