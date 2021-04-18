bd1d26685abf1b100b8107c31a3dc88f05d2f8c0
the-wulv <michaiac18@sch.gr>
2021-04-18 12:14:39 +0300

    Added basic style for the pad editor

--- src/renderer/components/PadEditor.jsx
+++ src/renderer/components/PadEditor.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 
+import './pad-editor.css'
+
 class PadEditor extends Component {
 	constructor(props){
 		super(props);
