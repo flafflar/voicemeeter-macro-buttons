13f98a1499745f8d9b799f25a00b635cb3b9aff9
the-wulv <michaiac18@sch.gr>
2021-04-25 10:06:16 +0300

    Made the getDLLPath synchronous
    This was made by directly calling the REG command, and not with the winreg package.
    Thus, the winreg package was removed

--- src/lib/voicemeeter-api.js
+++ src/lib/voicemeeter-api.js
@@ -1,29 +1,13 @@
 import path from 'path'
+import {execSync} from 'child_process'
 import ffi from 'ffi-napi'
-import Registry from 'winreg'
-
-/**
- * Callback for the getDLLPath method
- *
- * @callback getDLLPathCallback
- * @param {Error|null} err The error that occured during the call of the
- * function, or `null` if no errors occured
- * @param {string} path The path of the VoicemeeterRemote.dll
- */
 
 /**
  * Locates the VoicemeeterRemote.dll file and returns its path
- *
- * @async
- * @param {getDLLPathCallback} callback
  */
-function getDLLPath(callback){
-	let key = new Registry({
-		hive: Registry.HKLM,
-		key: '\\SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\VB:Voicemeeter {17359A74-1236-5467}'
-	})
-	key.get('UninstallString', (err, item) => {
-		if (err) return callback(err);
-		return callback(null, path.join(path.dirname(item.value), 'VoicemeeterRemote.dll'));
-	})
+function getDLLPath(){
+	let out = execSync('reg query "HKLM\\SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\VB:Voicemeeter {17359A74-1236-5467}" /v UninstallString');
+	let re = /UninstallString\s+REG_SZ\s+(?<value>.+)\r\n/;
+	let value = re.exec(out).groups.value;
+	return value;
 }
