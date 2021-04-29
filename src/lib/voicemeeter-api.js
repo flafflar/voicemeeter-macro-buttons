2f1f934ee037a97edce9be6c56e90d121750b258
the-wulv <michaiac18@sch.gr>
2021-04-29 12:53:39 +0300

    Created a class that represents a Strip
    Some parameters of the strips are not yet defined in the class

--- src/lib/voicemeeter-api.js
+++ src/lib/voicemeeter-api.js
@@ -632,6 +632,398 @@ API.getInputDeviceInfo = function getInputDeviceInfo(){
 
 // TODO: Implement VB-Audio Callback
 
+
+/**
+ * Represents a Voicemeeter strip
+ */
+class Strip {
+
+	/**
+	 * Constructs a strip
+	 * @param {Voicemeeter} voicemeeter The Voicemeeter instance that created
+	 * the strip
+	 * @param {number} index The index of the strip
+	 */
+	constructor(voicemeeter, index){
+
+		/**
+		 * The Voicemeeter instance that created the strip
+		 * @type {Voicemeeter}
+		 */
+		this.voicemeeter = voicemeeter;
+
+		/**
+		 * The index of the strip
+		 * @type {number}
+		 */
+		this.index = index;
+
+		/**
+		 * Whether the strip is virtual
+		 *
+		 * `true` if the strip is virtual, `false` if it is physical
+		 * @type {boolean}
+		 */
+		this.isVirtualStrip = index >= Strip.Lengths[voicemeeter.type].physical;
+
+		/**
+		 * Defines a property on `this` that automatically gets/sets the
+		 * corresponding property on Voicemeeter
+		 *
+		 * @private
+		 * @param {string} name The name of the property
+		 * @param {string} [vmName=name] The name of the Voicemeeter property (defaults to `name`)
+		 * @returns {void}
+		 */
+		const generateNumericProperty = (name, vmName) => Object.defineProperty(this, name, {
+			get: () => API.getParameter(`Strip[${this.index}].${vmName || name}`),
+			set: (value) => API.setParameter(`Strip[${this.index}].${vmName || name}`, Number(value))
+		});
+
+		/**
+		 * Defines a property on `this` that accepts a string value and
+		 * automatically gets/sets the corresponding property on Voicemeeter
+		 *
+		 * @private
+		 * @param {string} name The name of the property
+		 * @param {string} [vmName=name] The name of the Voicemeeter property (defaults to `name`)
+		 * @returns {void}
+		 */
+		 const generateStringProperty = (name, vmName) => Object.defineProperty(this, name, {
+			get: () => !!API.getParameter(`Strip[${this.index}].${vmName || name}`),
+			set: (value) => API.setParameter(`Strip[${this.index}].${vmName || name}`, String(value))
+		});
+
+		/**
+		 * Defines a property on `this` that accepts a boolean value and
+		 * automatically gets/sets the corresponding property on Voicemeeter
+		 *
+		 * @private
+		 * @param {string} name The name of the property
+		 * @param {string} [vmName=name] The name of the Voicemeeter property (defaults to `name`)
+		 * @returns {void}
+		 */
+		const generateBooleanProperty = (name, vmName) => Object.defineProperty(this, name, {
+			get: () => !!API.getParameter(`Strip[${this.index}].${vmName || name}`),
+			set: (value) => API.setParameter(`Strip[${this.index}].${vmName || name}`, value ? 1 : 0)
+		});
+
+		/**
+		 * Whether the strip is forced to be monophonic
+		 *
+		 * @name Strip#Mono
+		 * @type {boolean}
+		 */
+
+		/**
+		 * Whether the strip is muted
+		 *
+		 * @name Strip#Mute
+		 * @type {boolean}
+		 */
+
+		/**
+		 * Whether the strip is soloed
+		 *
+		 * @name Strip#Solo
+		 * @type {boolean}
+		 */
+
+		/**
+		 * Whether the center channel of the strip is muted
+		 *
+		 * @name Strip#MC
+		 * @type {boolean}
+		 */
+
+		/**
+		 * The value of the gain slider of the strip (in dB)
+		 *
+		 * Range of this value is -60 to +12 dB
+		 *
+		 * @name Strip#Gain
+		 * @type {number}
+		 */
+
+		// TODO: GainLayer
+
+		/**
+		 * Left-right pan of the strip
+		 *
+		 * Range of this value is -0.5 to +0.5
+		 *
+		 * @name Strip#Pan_x
+		 * @type {number}
+		 */
+
+		/**
+		 * Front-back pan of the strip
+		 *
+		 * On virtual strips (which are 7.1), this pans the surround audio back
+		 * and front. In that case, this value ranges between -0.5 and +0.5. On
+		 * physical strips, psychoacoustic effects are used to "move" the audio
+		 * away from the listener. In that case, this value ranges from 0 to 1.
+		 *
+		 * @name Strip#Pan_y
+		 * @type {number}
+		 */
+
+		/**
+		 * The x position of the color panel of the strip
+		 *
+		 * This value ranges from -0.5 to +0.5.
+		 *
+		 * Available only in physical strips
+		 *
+		 * @name Strip#Color_x
+		 * @type {number}
+		 */
+
+		/**
+		 * The y position of the color panel of the strip
+		 *
+		 * This value ranges from 0 to 1.
+		 *
+		 * Available only in physical strips
+		 *
+		 * @name Strip#Color_y
+		 * @type {number}
+		 */
+
+		/**
+		 * The x position of the effect panel of the strip
+		 *
+		 * This value ranges from -0.5 to +0.5.
+		 *
+		 * Available only in physical strips
+		 *
+		 * @name Strip#Color_x
+		 * @type {number}
+		 */
+
+		/**
+		 * The y position of the effect panel of the strip
+		 *
+		 * This value ranges from 0 to 1.
+		 *
+		 * Available only in physical strips
+		 *
+		 * @name Strip#Color_y
+		 * @type {number}
+		 */
+
+		/**
+		 * The audibility of the strip
+		 *
+		 * Available only in simple Voicemeeter
+		 *
+		 * @name Strip#Audibility
+		 * @type {number}
+		 */
+
+		/**
+		 * The compression level of the strip
+		 *
+		 * Available only in Voicemeeter Banana and Potato
+		 *
+		 * @name Strip#Comp
+		 * @type {number}
+		 */
+
+		/**
+		 * The gate level of the strip
+		 *
+		 * Available only in Voicemeeter Banana and Potato
+		 *
+		 * @name Strip#Gate
+		 * @type {number}
+		 */
+
+		/**
+		 * The karaoke algorithm of the strip
+		 *
+		 * Available only in virtual strips on Voicemeeter Banana and
+		 * Potato
+		 *
+		 * @name Strip#Karaoke
+		 * @type {number}
+		 */
+
+		/**
+		 * The limiter level of the strip
+		 *
+		 * Available only in Voicemeeter Banana and Potato
+		 *
+		 * @name Strip#Limit
+		 * @type {number}
+		 */
+
+		// TODO: Find which EQ number corresponds to what frequency
+
+		/**
+		 * The label of the strip
+		 *
+		 * @name Strip#Level
+		 * @type {string}
+		 */
+
+		/**
+		 * Whether the strip sends audio to output A1
+		 *
+		 * @name Strip#A1
+		 * @type {boolean}
+		 */
+
+		/**
+		 * Whether the strip sends audio to output A2
+		 *
+		 * Available only on Voicemeeter Banana and Potato
+		 *
+		 * @name Strip#A2
+		 * @type {boolean}
+		 */
+
+		/**
+		 * Whether the strip sends audio to output A3
+		 *
+		 * Available only on Voicemeeter Banana and Potato
+		 *
+		 * @name Strip#A3
+		 * @type {boolean}
+		 */
+
+		/**
+		 * Whether the strip sends audio to output A4
+		 *
+		 * Available only on Voicemeeter Potato
+		 *
+		 * @name Strip#A4
+		 * @type {boolean}
+		 */
+
+		/**
+		 * Whether the strip sends audio to output A5
+		 *
+		 * Available only on Voicemeeter Potato
+		 *
+		 * @name Strip#A5
+		 * @type {boolean}
+		 */
+
+		/**
+		 * Whether the audio is sent to the Reverb effect before or after the
+		 * fader
+		 *
+		 * Available only on Voicemeeter Potato
+		 *
+		 * @name Strip#PostReverb
+		 * @type {boolean}
+		 */
+
+		/**
+		 * Whether the audio is sent to the Delay effect before or after the
+		 * fader
+		 *
+		 * Available only on Voicemeeter Potato
+		 *
+		 * @name Strip#PostDelay
+		 * @type {boolean}
+		 */
+
+		/**
+		 * Whether the audio is sent to the FX1 effect before or after the
+		 * fader
+		 *
+		 * Available only on Voicemeeter Potato
+		 *
+		 * @name Strip#PostFx1
+		 * @type {boolean}
+		 */
+
+		/**
+		 * Whether the audio is sent to the FX2 effect before or after the
+		 * fader
+		 *
+		 * Available only on Voicemeeter Potato
+		 *
+		 * @name Strip#PostFx2
+		 * @type {boolean}
+		 */
+
+		[
+			'Mono',
+			'Mute',
+			'Solo',
+			'MC',
+			'A1',
+			voicemeeter.type >= API.Types.Banana && 'A2',
+			voicemeeter.type >= API.Types.Banana && 'A3',
+			voicemeeter.type >= API.Types.Potato && 'A4',
+			voicemeeter.type >= API.Types.Potato && 'A5',
+			'B1',
+			voicemeeter.type >= API.Types.Banana && 'B2',
+			voicemeeter.type >= API.Types.Potato && 'B3',
+			voicemeeter.type >= API.Types.Potato && 'PostReverb',
+			voicemeeter.type >= API.Types.Potato && 'PostDelay',
+			voicemeeter.type >= API.Types.Potato && 'PostFx1',
+			voicemeeter.type >= API.Types.Potato && 'PostFx2',
+		].filter(name => !!name).forEach(name => generateBooleanProperty(name));
+
+		[
+			'Gain',
+			'Pan_x',
+			'Pan_y',
+			!this.isVirtualStrip && 'Color_x',
+			!this.isVirtualStrip && 'Color_y',
+			!this.isVirtualStrip && voicemeeter.type >= API.Types.Banana && 'fx_x',
+			!this.isVirtualStrip && voicemeeter.type >= API.Types.Banana && 'fx_y',
+			voicemeeter.type == API.Types.Normal && 'Audibility',
+			voicemeeter.type >= API.Types.Banana && 'Comp',
+			voicemeeter.type >= API.Types.Banana && 'Gate',
+			voicemeeter.type >= API.Types.Banana && 'Karaoke',
+			voicemeeter.type >= API.Types.Banana && 'Limit',
+			this.isVirtualStrip && 'EQGain1',
+			this.isVirtualStrip && 'EQGain2',
+			this.isVirtualStrip && 'EQGain3',
+			voicemeeter.type >= API.Types.Potato && 'Reverb',
+			voicemeeter.type >= API.Types.Potato && 'Delay',
+			voicemeeter.type >= API.Types.Potato && 'Fx1',
+			voicemeeter.type >= API.Types.Potato && 'Fx2',
+		].filter(name => !!name).forEach(name => generateNumericProperty(name));
+
+		[
+			'Label'
+		].filter(name => !!name).forEach(name => generateStringProperty(name));
+
+	}
+}
+
+/**
+ * How many strips each type of Voicemeeter has
+ */
+Strip.Lengths = {
+	[API.Types.Normal]: {
+		physical: 2,
+		virtual: 1,
+		total: 3
+	},
+	[API.Types.Banana]: {
+		physical: 3,
+		virtual: 2,
+		total: 5
+	},
+	[API.Types.Potato]: {
+		physical: 5,
+		virtual: 3,
+		total: 8
+	},
+	[API.Types.Potato64]: {
+		physical: 5,
+		virtual: 3,
+		total: 8
+	}
+}
+
 /**
  * A handler for a connection to the Voicemeeter API
  */
